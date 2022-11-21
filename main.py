0ac0f5d2e571cc412a8b31080a6f35bf502546ce
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-21 10:40:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 
 
-from forex_python.converter import CurrencyRates
-
-c = CurrencyRates()
-
 amount = int(input('Enter amount: '))
 from_currency = input('From currency: ').upper()
 to_currency = input('To currency: ').upper()
